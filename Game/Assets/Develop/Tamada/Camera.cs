5283486a1a1419e2440a26d4a5221b737f61a6ad
kbc18b16 <kbc18b16@stu.kawahara.ac.jp>
2020-09-28 12:13:27 +0900

    プレイヤー簡易操作　カメラ追従　（仮

--- Game/Assets/Develop/Tamada/Camera.cs
+++ Game/Assets/Develop/Tamada/Camera.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    //private GameObject player;   //プレイヤー情報格納用
+    public Transform target;    //追従するターゲット。
+    public Vector3 offset;  //プレイヤーとカメラの距離。
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //this.player = GameObject.Find("Player1");
+
+        offset = new Vector3(-10.0f, 3.0f, 0.0f);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //カメラ向き
+        //transform.rotation = player.transform.rotation;
+        transform.position = target.position + offset;
+        this.transform.LookAt(target.position);
+
     }
 }
