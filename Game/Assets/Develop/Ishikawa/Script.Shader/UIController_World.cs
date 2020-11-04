49ea3b2428fb52a6899b236d1a89a2f3a83562d2
Yuuki Ishikawa <kbc18b01@stu.kawahara.ac.jp>
2020-11-04 09:29:30 +0900

    player

---
+++ Game/Assets/Develop/Ishikawa/Script.Shader/UIController_World.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController_World : MonoBehaviour
+{
+    private RectTransform myRectTfm;
+
+    void Start()
+    {
+        myRectTfm = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        // 自身の向きをカメラに向ける
+        myRectTfm.LookAt(Camera.main.transform);
+    }
+}
