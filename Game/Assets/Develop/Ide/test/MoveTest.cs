59e357892937a48f496aef2f18eabb83bcc17b57
ideGIT0427 <kbc18b02@stu.kawahara.ac.jp>
2021-01-22 11:36:22 +0900

    l

---
+++ Game/Assets/Develop/Ide/test/MoveTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MoveTest : MonoBehaviour
+{
+   public void OnClicStartButton()
+    {
+        SceneManager.LoadScene("titl0");
+
+    }
+}
---
+++ Game/Assets/Develop/Ide/test/TTSmove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TTSmove : MonoBehaviour
+{
+    public void OnClicStartButton()
+    {
+        SceneManager.LoadScene("Main0");
+
+    }
+
+}
