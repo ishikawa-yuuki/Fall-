e10351e24b1ef0a2f2ed98e0e3489105a72c9a0b
Yuuki Ishikawa <kbc18b01@stu.kawahara.ac.jp>
2021-02-05 09:49:16 +0900

    setシーンのバグ修正

--- Game/Assets/Develop/Ide/ほんへ/sMoveMenyu.cs
+++ Game/Assets/Develop/Ide/ほんへ/sMoveMenyu.cs
@@ -4,20 +4,33 @@ using Photon.Pun;
 public class sMoveMenyu : MonoBehaviour
 {
     private bool clic = false;
+    public bool test = false;
     public void OnClicStartButton()
     {
-        clic = true;
-    }
-    void Update()
-    {
-        if (!PhotonNetwork.IsConnected)
+        if (test)
         {
             SceneManager.LoadScene("Menyu");
         }
-        if (clic)
+        else
         {
-            //ネットワーク切断
-            PhotonNetwork.Disconnect();
+            clic = true;
         }
+       
+    }
+    void Update()
+    {
+        if (!test)
+        {
+            if (!PhotonNetwork.IsConnected)
+            {
+                SceneManager.LoadScene("Menyu");
+            }
+            if (clic)
+            {
+                //ネットワーク切断
+                PhotonNetwork.Disconnect();
+            }
+        }
+        
     }
 }
