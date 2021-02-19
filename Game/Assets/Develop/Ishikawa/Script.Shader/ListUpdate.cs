7b1093bf7ae8cd1b959f2f84f989fa3dc5398c86
Yuuki Ishikawa <kbc18b01@stu.kawahara.ac.jp>
2021-02-19 11:25:05 +0900

    修正用

--- Game/Assets/Develop/Ishikawa/Script.Shader/ListUpdate.cs
+++ Game/Assets/Develop/Ishikawa/Script.Shader/ListUpdate.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 using Photon.Pun;
 public class ListUpdate : MonoBehaviour
 {
+    public GameObject joinText;
     public void OnClicRetuButton()
     {
+        if (joinText.activeSelf)
+        {
+            return;
+        }
         PhotonNetwork.LeaveLobby();
         PhotonNetwork.JoinLobby();
         Debug.Log("List更新");
